927ab69fd478ca0053a5daa5f81fa4a06f8c3c8a
sanketkarvekar <sksanket7@gmail.com>
2021-01-03 11:05:25 +0530

    first commit

---
+++ welcome.js
@@ -0,0 +1,28 @@
+console.log("\nJavascript Hands-On");
+
+
+//Arrays In Javascript
+console.log("\nArrays In Javascript");
+var myArray = ["Hi", 2 , "sanket", '!'];    
+console.log(myArray[0].length);
+
+//Multi-Dimentional Arrays
+console.log("\nMulti-Dimentional Arrays");
+var multiArray = [["Hi", 2 , "sanket", '!'], ['*']];
+console.log(multiArray[1][0]);
+
+//push(<element>)
+console.log("\npush(<element>) --> Adds new element");
+multiArray.push(["Pushed Array"]);
+console.log(multiArray.length);
+
+//pop() --> removes the last element (newly added)
+console.log("\npop()  --> removes the last element (newly added)");
+multiArray.pop();
+console.log(multiArray.length);
+
+//shift() --> removes the first element 
+console.log("\nshift() --> removes the first element");
+multiArray.shift();
+console.log(multiArray.length);
+
